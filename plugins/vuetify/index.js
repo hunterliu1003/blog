44260fdc4bb892bb818ae03dd3b5ccb94ffecafb
Hunter <hunterliu1003@gmail.com>
2018-10-01 21:20:16 +0800

    add vuetify.config.js

---
+++ plugins/vuetify/index.js
@@ -0,0 +1,8 @@
+import Vue from 'vue'
+import Vuetify from 'vuetify'
+
+import theme from '@/vuetify.config.js'
+
+Vue.use(Vuetify, {
+  theme
+})
--- server/index.js
+++ server/index.js
@@ -4,7 +4,7 @@ const consola = require('consola')
 const { Nuxt, Builder } = require('nuxt')
 const app = express()
 const host = process.env.HOST || '127.0.0.1'
-const port = process.env.PORT || 3000
+const port = process.env.PORT || 8787
 
 app.set('port', port)
 
