0b04fd6438e43347863ef3b43776b87157dda31c
Hunter <hunterliu1003@gmail.com>
2018-10-03 13:56:30 +0800

    init layout admin

---
+++ layouts/admin.vue
@@ -0,0 +1,19 @@
+<template lang="pug">
+  v-app(dark)
+    v-toolbar.admin-header(fixed height="50")
+      nuxt-link.title.white--text.admin-title(nuxt to="/") Hunterliu
+      v-spacer
+      v-btn(icon title="登出" @click="signOut")
+        v-icon(size="20") fas fa-sign-out-alt
+    no-ssr
+      nuxt.admin-content
+</template>
+
+<style lang="stylus" scoped>
+  .admin-header
+    z-index 999
+  .admin-title
+    text-decoration none
+  .admin-content
+    margin-top 50px
+</style>
