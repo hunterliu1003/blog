fda7750c964bfd0d62a8d7d2d3b9060f321d078a
Hunter <hunterliu1003@gmail.com>
2018-10-03 13:56:03 +0800

    init layout error page

---
+++ layouts/error.vue
@@ -0,0 +1,28 @@
+<template lang="pug">
+  v-container.error-page
+    h1 Oop, something went wrong!
+    p Back to&nbsp;
+      a(href="/") safety
+      | &nbsp;!
+</template>
+
+<script>
+export default {
+  layout: 'default'
+}
+</script>
+
+<style lang="stylus" scoped>
+.error-page
+  text-align center
+  height 100%
+  display flex
+  flex-direction column
+  justify-content center
+a
+  text-decoration none
+  color red
+
+</style>
+
+
