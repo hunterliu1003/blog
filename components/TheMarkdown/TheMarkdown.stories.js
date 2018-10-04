c0ddde8aee83f62acb2a459a9ba8ef641edb2f30
Hunter <hunterliu1003@gmail.com>
2018-10-04 20:14:17 +0800

    update md text

--- components/TheMarkdown/TheMarkdown.stories.js
+++ components/TheMarkdown/TheMarkdown.stories.js
@@ -15,53 +15,26 @@ storiesOf('TheMarkdown', module)
 .add('TheMarkdown', withReadme(README, () => ({
   data: vm => ({
     md: `
+      \n# 以下都是 markdown:
       \n<div class="red--text" style="font-size:3em;">HTML div</div>
-\n
       \n# \`用範例理解\`  Vue.js 目錄222
       \n## 用範例理解  Vue.js 目錄ddddd
       \n### 用範例理解  Vue.js 目錄
       \n#### 用範例理解  Vue.js 目錄d
       \n##### 用範例理解  Vuedddddddjs 目錄
       \n###### 用範例理解  Vue.js 目錄{.red--text}
-\n
-\n
       \n# Image
-\n
       \n![](https://store.storeimages.cdn-apple.com/8750/as-images.apple.com/is/image/AppleInc/aos/published/images/m/ac/macbook/select/macbook-select-space-gray-201706?wid=452&hei=420&fmt=jpeg&qlt=95&op_usm=0.5,0.5&.v=1505775431709){width=100}
-\n
       \n# Highlightjs
-\n
       \n## Bash
-\n
-      \n\`\`\`bash
-      \n# dasfasd
-      \n\`\`\`
-\n
+      \n\`\`\`bash\n# dasfasd\n\`\`\`
       \n## HTML
-\n
-      \n\`\`\`html
-      \n<div id="app">
-      \n  <p>{{ message }}</p>
-      \n</div>
-      \n\`\`\`
-\n
+      \n\`\`\`html\n<div id="app">\n<p>{{ message }}</p>\n</div>\n\`\`\`
       \n## Javascript
-\n
-      \n\`\`\`javascript
-      \nnew Vue({
-      \n  el: '#app',
-      \n  data: {
-      \n    message: 'Hello Vue.js!'
-      \n  }
-      \n})
-      \n\`\`\`
-\n
+      \n\`\`\`javascript\nnew Vue({\n  el: '#app',\n  data: {\n    message: 'Hello Vue.js!'\n  }\n})\n\`\`\`\n
       \n# Iframe
-\n
       \n## Storybook
-\n
       \n<iframe frameborder='no'  scrolling='no' src="http://hunterliu1003.github.io/nuxt-blog-storybook/iframe.html?selectedKind=TheInputTags&selectedStory=TheInputTags&full=0&addons=1&stories=1&panelRight=1&addonPanel=REACT_STORYBOOK%2Freadme%2Fpanel"></iframe>
-\n
       \n@[codepen](https://codepen.io/hunterliu1003/embed/zjBBpK/?height=265&amp;theme-id=0&amp;default-tab=js,result&amp;embed-version=2)
       \n@[jsfiddle](https://jsfiddle.net/rykeller/y4848ak7/8/embedded/html,css,result/)
       \n<iframe height='300' scrolling='no' title='Vuetify Example Pen' src='https://codepen.io/hunterliu1003/embed/zjBBpK/?height=300&theme-id=33425&default-tab=html&embed-version=2' frameborder='no' allowtransparency='true' allowfullscreen='true' style='width: 100%;'>See the Pen <a href='https://codepen.io/hunterliu1003/pen/zjBBpK/'>Vuetify Example Pen</a> by Hunter (<a href='https://codepen.io/hunterliu1003'>@hunterliu1003</a>) on <a href='https://codepen.io'>CodePen</a>.
