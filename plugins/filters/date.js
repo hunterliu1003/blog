0ca87b71a1698bf704bcc400aa7ebb06954ce4d7
Hunter <hunterliu1003@gmail.com>
2018-10-07 23:32:25 +0800

    add filter date

---
+++ plugins/filters/date.js
@@ -0,0 +1,30 @@
+import Vue from 'vue'
+
+const months = [
+  'Jan',
+  'Feb',
+  'Mar',
+  'Apr',
+  'May',
+  'Jun',
+  'Jul',
+  'Aug',
+  'Sep',
+  'Oct',
+  'Nov',
+  'Dec'
+]
+const dateFilter = value => {
+  return formatDate(value)
+}
+
+function formatDate (inputDate) {
+  const date = new Date(inputDate)
+  const year = date.getFullYear()
+  const month = date.getMonth()
+  const day = date.getDate()
+  const formattedDate = day + '. ' + months[month] + ' ' + year
+  return formattedDate
+}
+
+Vue.filter('date', dateFilter)
