ca197cb481569c401522d08e4c55b7e2f8653863
Hunter <hunterliu1003@gmail.com>
2018-10-08 19:01:30 +0800

    add vuex module post & tags

---
+++ store/modules/post.js
@@ -0,0 +1,76 @@
+import { db } from '@/services/fireinit'
+
+export const actions = {
+  addPost ({}, payload) {
+    return db.collection('posts').add({ ...payload })
+      .then(data => ({ id: data.id, ...payload }))
+  },
+  setPost ({}, payload) {
+    return db.collection('posts').doc(payload.postId).set({ ...payload.thePost }, { merge: true })
+      .then(() => ({ ...payload.thePost, id: payload.postId }))
+  },
+  async getPostsByPage ({}, payload) {
+    let postsCol = db.collection('posts').where('isShow', '==', true).orderBy('postTime', 'desc')
+    let docs = await postsCol.get()
+      .then(({ docs }) => docs)
+    let startAt = (payload.pageNum - 1) * payload.perPage
+    let page = docs[startAt] ? Number(payload.pageNum) : 1
+    let size = docs.length
+    let pageLength = Math.ceil(size / payload.perPage)
+    if (size === 0) return {
+      page: 1,
+      size: 1,
+      posts: []
+    }
+    let postTime = docs[startAt]
+    ? docs[startAt].data().postTime
+    : docs[0].data().postTime
+
+    return await postsCol.startAt(postTime).limit(payload.perPage).get()
+      .then(({ docs }) => {
+        const posts = []
+        docs.forEach(doc => {
+          posts.push({ ...doc.data(), id: doc.id })
+        })
+        return {
+          page,
+          pageLength,
+          posts
+        }
+      })
+  },
+  getPostsAdmin () {
+    return db.collection('posts').orderBy('postTime', 'desc').get()
+      .then(docs => {
+        const posts = []
+        docs.forEach(doc => {
+          posts.push({ ...doc.data(), id: doc.id })
+        })
+        return posts
+      })
+  },
+  getPostsTag ({}, tagId) {
+    return db.collection('posts').where('isShow', '==', true).where('tags', 'array-contains', tagId).get()
+      .then(docs => {
+        const posts = []
+        docs.forEach(doc => {
+          posts.push({ ...doc.data(), id: doc.id })
+        })
+        return posts
+      })
+  },
+  getPostByPostId ({}, postId) {
+    return db.collection('posts').doc(postId).get()
+      .then(doc => doc.data())
+  },
+  getPosts () {
+    return db.collection('posts').where('isShow', '==', true).orderBy('postTime', 'desc').get()
+      .then(({ docs }) => {
+        const posts = []
+        docs.forEach(doc => {
+          posts.push({ ...doc.data(), id: doc.id })
+        })
+        return posts
+      })
+  }
+}
---
+++ store/modules/tags.js
@@ -0,0 +1,26 @@
+import { db } from '@/services/fireinit'
+import { uniq, flatten } from 'lodash'
+
+export const actions = {
+  getAllTags () {
+    let postsCol = db.collection('posts').where('isShow', '==', true)
+    return postsCol.orderBy('postTime', 'desc').get()
+      .then(({ docs }) => {
+        let allTags = []
+        docs.forEach(doc => {
+          allTags.push(doc.data().tags)
+        })
+        allTags = uniq(flatten(allTags))
+        const promises = allTags.map(tag => {
+          return postsCol.where('tags', 'array-contains', tag).get()
+            .then(({ docs }) => {
+              return {
+                name: tag,
+                size: docs.length,
+              }
+            })
+        })
+        return Promise.all(promises)
+      })
+  }
+}
