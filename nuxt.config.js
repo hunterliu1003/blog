92b2e1aaad7699c143d4da5d9548fb83ca4a5766
Hunter <hunterliu1003@gmail.com>
2018-10-11 11:24:12 +0800

    add nuxt config head titleTemplate

--- nuxt.config.js
+++ nuxt.config.js
@@ -7,7 +7,9 @@ module.exports = {
   ** Headers of the page
   */
   head: {
-    title: pkg.name,
+    titleTemplate: titleChunk => {
+      return titleChunk ? `${ titleChunk } | Hunterliu's Blog` : `Hunterliu's Blog`
+    },
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
