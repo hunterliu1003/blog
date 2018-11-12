419e875f19e132fd13b0e213eb3339daca699d9d
Hunter <hunterliu1003@gmail.com>
2018-11-12 12:15:57 +0800

    rewrite storybook webpack config with Full Control Mode

--- .storybook/webpack.config.js
+++ .storybook/webpack.config.js
@@ -1,30 +1,34 @@
 const path = require("path");
 
-module.exports = {
-  module: {
-    rules: [
-      {
-        test: /\.stories\.jsx?$/,
-        loaders: [require.resolve('@storybook/addon-storysource/loader')],
-        enforce: 'pre'
-      },
-      {
-        test: /\.pug$/,
-        loader: 'pug-plain-loader'
-      },
-      {
-        test: /\.styl(us)?$/,
-        use: [
-          'vue-style-loader',
-          'css-loader',
-          'stylus-loader'
-        ]
-      }
+// Export a function. Accept the base config as the only param.
+module.exports = (storybookBaseConfig, configType) => {
+  // configType has a value of 'DEVELOPMENT' or 'PRODUCTION'
+  // You can change the configuration based on that.
+  // 'PRODUCTION' is used when building the static version of storybook.
+
+  // Make whatever fine-grained changes you need
+  storybookBaseConfig.module.rules.push({
+    test: /\.stories\.jsx?$/,
+    loaders: [require.resolve('@storybook/addon-storysource/loader')],
+    enforce: 'pre'
+  });
+
+  storybookBaseConfig.module.rules.push({
+    test: /\.pug$/,
+    loader: 'pug-plain-loader'
+  });
+
+  storybookBaseConfig.module.rules.push({
+    test: /\.styl(us)?$/,
+    use: [
+      'vue-style-loader',
+      'css-loader',
+      'stylus-loader'
     ]
-  },
-  resolve: {
-    alias: {
-      '@': path.dirname(path.resolve(__dirname))
-    }
-  }
+  });
+
+  storybookBaseConfig.resolve.alias['@'] = path.dirname(path.resolve(__dirname))
+
+  // Return the altered config
+  return storybookBaseConfig;
 };
