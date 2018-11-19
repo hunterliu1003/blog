34e83a579380160a8275076ad8e6af3ac53b7783
Hunter <hunterliu1003@gmail.com>
2018-11-19 17:41:26 +0800

    change vuetify config path

--- plugins/vuetify/index.js
+++ plugins/vuetify/index.js
@@ -1,7 +1,7 @@
 import Vue from 'vue'
 import Vuetify, { VLayout } from 'vuetify/lib'
 
-import theme from '@/vuetify.config.js'
+import theme from './vuetify.config.js'
 
 Vue.use(Vuetify, {
   theme,
