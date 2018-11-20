e707bd67f3b95d5d175ab27590f392a54e07f18a
Hunter <hunterliu1003@gmail.com>
2018-11-20 13:36:52 +0800

    not important

--- pages/about/index.vue
+++ pages/about/index.vue
@@ -1,28 +1,34 @@
 <template lang="pug">
-  v-container(fluid).ma-0
+  v-container.ma-0(fluid)
     v-layout.mb-4
       v-flex(xs12 sm10 offset-sm1 md12 offset-md0 lg9)
         h1.text-xs-center.display-1.font-ch 關於我
-    v-layout(column)
+    v-layout
       v-flex.subheading(xs12 sm10 offset-sm1 md12 offset-md0 lg9)
         p 我是 Hunter，是個前端工程師，不是獵頭， 
         p 熱愛研究和分享前端技術，並朝年薪百萬︎的路邁進。
         p Github 連結請點擊右上角。
         p 歡迎大家互相交流，若文章中有錯誤或不清楚的地方，歡迎提出與補充。
         p.headline 以下是我的學習歷程：
+    v-layout
       v-flex(xs12 sm10 offset-sm1 md12 offset-md0 lg9)
         v-timeline(:dense="$vuetify.breakpoint.smAndDown")
           v-timeline-item(v-for="(event, i) in events" :color="colors[i % colors.length]" :key="i" small)
-            .py-3
-              h2(:class="`headline font-weight-bold mb-1 ${colors[i % colors.length]}--text`") {{ event.title }}
-              div(:class="`font-weight-bold mb-1 ${colors[i % colors.length]}--text`" ) {{ event.date | date }}
-              div.subheading {{ event.content }}
+            .py-2
+              h2(:class="`headline font-weight-bold mb-1 ${colors[i % colors.length]}--text`")
+                | {{ event.title }}
+              div(:class="`font-weight-bold mb-1 ${colors[i % colors.length]}--text`" )
+                | {{ event.date | date }}
+              .subheading {{ event.content }}
 </template>
 
 <script>
 export default {
   data: () => ({
-    colors: ['cyan', 'green', 'pink', 'amber', 'orange', 'light-green', 'red', 'blue', 'yellow', 'teal'],
+    colors: [
+      'cyan', 'green', 'pink', 'amber', 'orange', 
+      'light-green', 'red', 'blue', 'yellow', 'teal'
+    ],
     events: [
       {
         title: '參加 JSDC 2018',
