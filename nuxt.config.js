c1189c9a03bd442705231e9fe3a714e2fb7416fc
Hunter <hunterliu1003@gmail.com>
2018-11-21 18:40:19 +0800

    add http2

--- nuxt.config.js
+++ nuxt.config.js
@@ -113,6 +113,12 @@ module.exports = {
       }
     }
   },
+  
+  render: {
+    http2: {
+      push: true
+    }
+  },
 
   ...routerBase,
 
--- plugins/vuetify/index.js
+++ plugins/vuetify/index.js
@@ -1,11 +1,8 @@
 import Vue from 'vue'
-import Vuetify, { VLayout } from 'vuetify/lib'
+import Vuetify from 'vuetify/lib'
 
 import theme from './vuetify.config.js'
 
 Vue.use(Vuetify, {
-  theme,
-  components: {
-    VLayout
-  },
+  theme
 })
