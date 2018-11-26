219133cf525567e2e3e85f33e58e87ea7e5f3240
Hunter <hunterliu1003@gmail.com>
2018-11-26 21:33:35 +0800

    update storybook webpack config

--- .storybook/webpack.config.js
+++ .storybook/webpack.config.js
@@ -1,23 +1,15 @@
-const path = require("path");
+const path = require("path")
 
-// Export a function. Accept the base config as the only param.
 module.exports = (storybookBaseConfig, configType) => {
-  // configType has a value of 'DEVELOPMENT' or 'PRODUCTION'
-  // You can change the configuration based on that.
-  // 'PRODUCTION' is used when building the static version of storybook.
-
-  // Make whatever fine-grained changes you need
   storybookBaseConfig.module.rules.push({
     test: /\.stories\.jsx?$/,
     loaders: [require.resolve('@storybook/addon-storysource/loader')],
     enforce: 'pre'
-  });
-
+  })
   storybookBaseConfig.module.rules.push({
     test: /\.pug$/,
     loader: 'pug-plain-loader'
   });
-
   storybookBaseConfig.module.rules.push({
     test: /\.styl(us)?$/,
     use: [
@@ -25,10 +17,8 @@ module.exports = (storybookBaseConfig, configType) => {
       'css-loader',
       'stylus-loader'
     ]
-  });
-
+  })
   storybookBaseConfig.resolve.alias['@'] = path.dirname(path.resolve(__dirname))
 
-  // Return the altered config
-  return storybookBaseConfig;
-};
+  return storybookBaseConfig
+}
